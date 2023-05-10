6e2d17bae8067c31142c112914d6bf660927030a
Daniel Oh <doh@redhat.com>
2023-05-10 15:25:55 +0100

    Remove nativeTest

--- quarkus-petclinic-visits-service/src/test/java/org/acme/rest/NativeVisitsResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme.rest;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeVisitsResourceIT extends VisitsResourceTest {
-
-    // Execute the same tests but in native mode.
-}
