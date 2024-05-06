fad11fba6d1c9a78ac75ab9c0b434f4e95f464c6
Daniel Oh <doh@redhat.com>
2024-05-05 20:17:17 -0600

    RHBQ 3.8, OpenShift 4.15, Dev Spaces 3.13

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -21,7 +21,7 @@ sh ${PROJECT_SOURCE}/scripts/deploy-customers-service.sh
 sh ${PROJECT_SOURCE}/scripts/deploy-visits-service.sh
 sh ${PROJECT_SOURCE}/scripts/deploy-petclinic-web-v2.sh
 
-oc rollout status -w dc/vets-service &&
-  oc rollout status -w dc/customers-service &&
-  oc rollout status -w dc/visits-service &&
-  oc rollout status -w dc/petclinic-web-v2
+oc rollout status -w deployment/vets-service &&
+  oc rollout status -w deployment/customers-service &&
+  oc rollout status -w deployment/visits-service &&
+  oc rollout status -w deployment/petclinic-web-v2
--- scripts/deploy-remain.sh
+++ scripts/deploy-remain.sh
@@ -16,6 +16,6 @@ sh ${PROJECT_SOURCE}/scripts/deploy-customers-service.sh
 sh ${PROJECT_SOURCE}/scripts/deploy-visits-service.sh
 sh ${PROJECT_SOURCE}/scripts/deploy-petclinic-web-v2.sh
 
-oc rollout status -w dc/customers-service &&
-  oc rollout status -w dc/visits-service &&
-  oc rollout status -w dc/petclinic-web-v2
+oc rollout status -w deployment/customers-service &&
+  oc rollout status -w deployment/visits-service &&
+  oc rollout status -w deployment/petclinic-web-v2
